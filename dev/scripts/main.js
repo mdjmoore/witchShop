8603821df1ff274c76fad89483d00fb13177ec8e
Meagan Moore <816moore@gmail.com>
2018-03-20 12:46:46 -0400

    inital commit

---
+++ dev/scripts/main.js
@@ -0,0 +1,12 @@
+const app = {};
+
+app.events = function () {
+};
+
+app.init = function () {
+    app.events();
+};
+
+$(function () {
+    app.init();
+});
---
+++ public/scripts/main.js
@@ -0,0 +1,13 @@
+"use strict";
+
+var app = {};
+
+app.events = function () {};
+
+app.init = function () {
+    app.events();
+};
+
+$(function () {
+    app.init();
+});
