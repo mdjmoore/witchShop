1c52bd4846868277afcfa7200f5bb5faf1c3c907
Meagan Moore <816moore@gmail.com>
2018-03-26 17:33:34 -0400

    html done starting styles

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -1,6 +1,12 @@
 const app = {};
 
 app.events = function () {
+    //convert to vanilla javascript
+    // $(document).ready(function () {
+    //     $(".hamburger").click(function () {
+    //         $(this).toggleClass("is-active");
+    //     });
+    // });
 };
 
 app.init = function () {
--- public/scripts/main.js
+++ public/scripts/main.js
@@ -2,7 +2,14 @@
 
 var app = {};
 
-app.events = function () {};
+app.events = function () {
+    //convert to vanilla javascript
+    // $(document).ready(function () {
+    //     $(".hamburger").click(function () {
+    //         $(this).toggleClass("is-active");
+    //     });
+    // });
+};
 
 app.init = function () {
     app.events();
