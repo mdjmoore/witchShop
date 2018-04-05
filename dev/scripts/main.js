7e4684b5e5589087ae46edf258d4feef98f32abd
Meagan Moore <816moore@gmail.com>
2018-04-04 20:48:50 -0400

    final commit, made cart dynamic

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -1,18 +1,111 @@
-const app = {};
+const witchShop = {};
 
-app.events = function () {
-    //convert to vanilla javascript
-    // $(document).ready(function () {
-    //     $(".hamburger").click(function () {
-    //         $(this).toggleClass("is-active");
-    //     });
-    // });
+witchShop.products= {
+
+        bravery: {
+            price: 23,
+            description: "liquid courage"
+        },
+        love: {
+            price: 37,
+            description: "love potion no.8"
+        },
+        fortune: {
+            price: 25,
+            description: "liquid luck"
+        },
+        money: {
+            price: 18,
+            description: "potion of good fortune"
+        },
+
+        luck: {
+            price: 17,
+            description: "charm of good luck"
+        },
+        allure: {
+            price: 22,
+            description: "charm of allure"
+        },
+        duplicity: {
+            price: 31,
+            description: "charm of duplicity"
+        },
+        protection: {
+            price: 26,
+            description: "charm of protection"
+        },
+
+        minor: {
+            price: 56,
+            description: "minor inconvenience"
+        },
+        major: {
+            price: 88,
+            description: "major inconvenience"
+        },
+        sleep: {
+            price: 45,
+            description: "sleeping draught"
+        },
+        confusion: {
+            price: 38,
+            description: "curse of confusion"
+        },
+}
+
+const cartTag = document.querySelector(".cart");
+const cartModal = document.querySelector("#shopping-cart");
+const close = document.querySelector(".close");
+const buyBtns = document.querySelectorAll(".add-to-cart");
+const cartItems = document.querySelector(".shopping-cart-items");
+const costList = document.querySelector(".cost");
+const total = document.querySelector(".total");
+const addedItem = document.getElementsByName("add");
+let itemID;
+let itemPrice;
+
+
+// function totalCost() {
+//     console.log(itemPrice);
+// }
+
+
+
+witchShop.events = function () {
+    cartTag.onclick = function () { showCart() };
+    close.onclick = function () { closeModal() };
+    buyBtns.forEach(buyBtn => buyBtn.addEventListener('click', handleUpdate));
+    function showCart() {
+        cartModal.style.display = "block";
+    };
+
+    function closeModal() {
+        cartModal.style.display = "none";
+    };
+    function handleUpdate() {
+        itemID = this.id;
+        itemPrice = witchShop.products[itemID].price;
+        // console.log(itemID);
+        addToCart(itemID, itemPrice);
+    }
+    function addToCart() {
+        // console.log(itemID);
+        const listItem = document.createElement("LI");
+        const listText = document.createTextNode(witchShop.products[itemID].description);
+        listItem.appendChild(listText);
+        cartItems.appendChild(listItem);
+        // console.log(witchShop.products[itemID].price);
+        const listItemPrice = document.createElement("LI");
+        const listPriceText = document.createTextNode(itemPrice);
+        listItemPrice.appendChild(listPriceText);
+        costList.appendChild(listItemPrice);
+        totalCost(itemPrice);
+    };
 };
 
-app.init = function () {
-    app.events();
+witchShop.init = function () {
+    witchShop.events();
 };
 
-$(function () {
-    app.init();
-});
+witchShop.init();
--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,20 +1,117 @@
 "use strict";
 
-var app = {};
+var witchShop = {};
 
-app.events = function () {
-    //convert to vanilla javascript
-    // $(document).ready(function () {
-    //     $(".hamburger").click(function () {
-    //         $(this).toggleClass("is-active");
-    //     });
-    // });
+witchShop.products = {
+
+    bravery: {
+        price: 23,
+        description: "liquid courage"
+    },
+    love: {
+        price: 37,
+        description: "love potion no.8"
+    },
+    fortune: {
+        price: 25,
+        description: "liquid luck"
+    },
+    money: {
+        price: 18,
+        description: "potion of good fortune"
+    },
+
+    luck: {
+        price: 17,
+        description: "charm of good luck"
+    },
+    allure: {
+        price: 22,
+        description: "charm of allure"
+    },
+    duplicity: {
+        price: 31,
+        description: "charm of duplicity"
+    },
+    protection: {
+        price: 26,
+        description: "charm of protection"
+    },
+
+    minor: {
+        price: 56,
+        description: "minor inconvenience"
+    },
+    major: {
+        price: 88,
+        description: "major inconvenience"
+    },
+    sleep: {
+        price: 45,
+        description: "sleeping draught"
+    },
+    confusion: {
+        price: 38,
+        description: "curse of confusion"
+    }
 };
 
-app.init = function () {
-    app.events();
+var cartTag = document.querySelector(".cart");
+var cartModal = document.querySelector("#shopping-cart");
+var close = document.querySelector(".close");
+var buyBtns = document.querySelectorAll(".add-to-cart");
+var cartItems = document.querySelector(".shopping-cart-items");
+var costList = document.querySelector(".cost");
+var total = document.querySelector(".total");
+var addedItem = document.getElementsByName("add");
+var itemID = void 0;
+var itemPrice = void 0;
+
+// function totalCost() {
+//     console.log(itemPrice);
+// }
+
+
+witchShop.events = function () {
+    cartTag.onclick = function () {
+        showCart();
+    };
+    close.onclick = function () {
+        closeModal();
+    };
+    buyBtns.forEach(function (buyBtn) {
+        return buyBtn.addEventListener('click', handleUpdate);
+    });
+    function showCart() {
+        cartModal.style.display = "block";
+    };
+
+    function closeModal() {
+        cartModal.style.display = "none";
+    };
+    function handleUpdate() {
+        itemID = this.id;
+        itemPrice = witchShop.products[itemID].price;
+        // console.log(itemID);
+        addToCart(itemID, itemPrice);
+    }
+    function addToCart() {
+        // console.log(itemID);
+        var listItem = document.createElement("LI");
+        var listText = document.createTextNode(witchShop.products[itemID].description);
+        listItem.appendChild(listText);
+        cartItems.appendChild(listItem);
+        // console.log(witchShop.products[itemID].price);
+        var listItemPrice = document.createElement("LI");
+        var listPriceText = document.createTextNode(itemPrice);
+        listItemPrice.appendChild(listPriceText);
+        costList.appendChild(listItemPrice);
+        totalCost(itemPrice);
+    };
 };
 
-$(function () {
-    app.init();
-});
+witchShop.init = function () {
+    witchShop.events();
+};
+
+witchShop.init();
